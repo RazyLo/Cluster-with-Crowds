108118c1dcf6757ba171d7606e2305a0edef4d7f
RazyLo <razylo@hotmail.com>
2014-06-16 17:03:37 +1000

    Update Start.java

--- src/Start/Start.java
+++ src/Start/Start.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import kmeans.KMean;
 
 public class Start {
-	//private static ArrayList<String> imageUrls;
+	//Entry of this application
 	public static void main(String[] args) throws IOException
 	{
 		KMean start = new KMean(2,"G:\\data.txt");
