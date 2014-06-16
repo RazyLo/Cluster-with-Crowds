f0e1ac8f6c632f840345e0d4e3663d61d21ec011
RazyLo <razylo@hotmail.com>
2014-06-16 17:12:38 +1000

    Update Distance.java

--- src/kmeans/Distance.java
+++ src/kmeans/Distance.java
@@ -1,37 +1,44 @@
 package kmeans;
 
 import java.io.IOException;
+
+/*
+ * This class is used to calculate the distance between source image and 
+ * destination image
+ * */
 public class Distance {
 	
 	private Image centerImage;
 	private Image sourceImage;
 	private double distance;
 	
-	public Distance(){
+	public Distance()
+	{
 		
 	}
 
-	public Distance(Image centerPoint,Image sourcePoint){
+	//Two images is required, the central image and the sourceImage
+	public Distance(Image centerPoint,Image sourcePoint)
+	{
 		this.centerImage = centerPoint;
 		this.sourceImage = sourcePoint;
-		//this.distance = distance;
 	}
 	
 
-	
+	//Calculate the distance between two images
 	public void caculateDis() throws IOException{
 		double tempDis = 0;
-		//double distance = 0;
-			for(int i=0;i<centerImage.getImageFeatureSize();i++)
-			{
-				double tempX = 0;
-				tempX = (centerImage.getImageFeature().get(i) - sourceImage.getImageFeature().get(i));
-				tempDis += (Math.pow(tempX, 2));
-			}
-				distance =Math.sqrt(tempDis);
+		for(int i=0;i<centerImage.getImageFeatureSize();i++)
+		{
+			double tempX = 0;
+			tempX = (centerImage.getImageFeature().get(i) - sourceImage.getImageFeature().get(i));
+			tempDis += (Math.pow(tempX, 2));
+		}
+			distance =Math.sqrt(tempDis);
 	}
 	
-	public double getDistance(){
+	public double getDistance()
+	{
 		return distance;
 	}
 	
@@ -40,7 +47,4 @@ public class Distance {
 		String temp = String.valueOf(distance);
 		return temp;
 	}
-	
-
-
 }
